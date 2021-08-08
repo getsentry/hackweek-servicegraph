f465988af5362663e8d2274f60684890af0a7c79
Vlad Kluev <vlad.kluev@sentry.io>
2021-08-08 09:31:04 -0700

    make the server except JSON and echo it

--- rust/servicegraph-api/src/main.rs
+++ rust/servicegraph-api/src/main.rs
@@ -1,11 +1,37 @@
 #[macro_use] extern crate rocket;
 
+use std::borrow::Cow;
+use rocket::serde::json::{Json, Value, json};
+use rocket::serde::{Serialize, Deserialize, };
+
+type Id = usize;
+
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct GraphTrace<'r> {
+    id: Id,
+    message: Cow<'r, str>
+}
+
+
+#[post("/log_trace", format = "json", data = "<graph_trace>")]
+fn log_trace(graph_trace: Json<GraphTrace>) -> Json<GraphTrace> {
+    println!("Received trace with id {} message {}", graph_trace.id, graph_trace.message);
+    return graph_trace;
+}
+
+
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/", routes![log_trace])
 }
