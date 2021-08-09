608ac72955f2960e09110fbe1cd91c13d89e714b
Armin Ronacher <armin.ronacher@active-4.com>
2021-08-09 23:09:11 +0200

    ref: cargo fmt

--- rust/servicegraph-api/src/db.rs
+++ rust/servicegraph-api/src/db.rs
@@ -2,32 +2,31 @@ use clickhouse::{error::Result, Client, Row};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-
-#[derive(Debug, Clone, PartialEq)]
-#[derive(Serialize, Deserialize, Row)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Row)]
 struct Node {
     id: [u8; 16],
-    service_name: String
+    service_name: String,
 }
 
 fn get_connection() -> Client {
     Client::default()
-    .with_url("http://localhost:8123")
-    .with_database("servicegraph")
+        .with_url("http://localhost:8123")
+        .with_database("servicegraph")
 }
-
 
 async fn register_node(connection: Client, node: Node) -> Result<()> {
     let mut insert = connection.insert("nodes")?;
     insert.write(&node).await?;
     insert.end().await
-
 }
 
 #[tokio::test]
 async fn test_register_node() {
     let client = get_connection();
     let uuid = *Uuid::parse_str(&"a".repeat(32)).unwrap().as_bytes();
-    let node = Node { id: uuid, service_name: "test-service".to_string() };
+    let node = Node {
+        id: uuid,
+        service_name: "test-service".to_string(),
+    };
     register_node(client, node).await.unwrap();
 }
--- rust/servicegraph-api/src/main.rs
+++ rust/servicegraph-api/src/main.rs
@@ -1,92 +1,94 @@
 mod payloads;
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 mod db;
 
-use std::collections::HashMap;
+use rocket::serde::json::{json, Json, Value};
+use rocket::serde::{Deserialize, Serialize};
 use std::borrow::Cow;
-use rocket::serde::json::{Json, Value, json};
-use rocket::serde::{Serialize, Deserialize, };
+use std::collections::HashMap;
 
 type Id = usize;
-
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct GraphTrace<'r> {
     id: Id,
-    message: Cow<'r, str>
+    message: Cow<'r, str>,
 }
-
 
 #[post("/log_trace", format = "json", data = "<graph_trace>")]
 fn log_trace(graph_trace: Json<GraphTrace>) -> Json<GraphTrace> {
-    println!("Received trace with id {} message {}", graph_trace.id, graph_trace.message);
+    println!(
+        "Received trace with id {} message {}",
+        graph_trace.id, graph_trace.message
+    );
     return graph_trace;
 }
 static nodes: [&str; 4] = [
-    "418f3d00-ba14-42eb-98b8-5f3fb1b975c8", 
-    "5042546b-07a0-41d4-a73c-9138722eebb4", 
-    "9ac770a6-0a2b-4805-b506-d87ba2510102", 
-    "7ce12aae-0acd-4e9b-a145-7848fddb1bfe"
+    "418f3d00-ba14-42eb-98b8-5f3fb1b975c8",
+    "5042546b-07a0-41d4-a73c-9138722eebb4",
+    "9ac770a6-0a2b-4805-b506-d87ba2510102",
+    "7ce12aae-0acd-4e9b-a145-7848fddb1bfe",
 ];
 
 #[get("/graph/<project_id>")]
 fn get_graph(project_id: u64) -> Json<payloads::GraphPayload> {
     let mut adjacency_map = HashMap::new();
     adjacency_map.insert(
-        String::from(nodes[0]), vec![
-            payloads::ConnectionInfo{
-                to_node: String::from(nodes[1]), 
-                hitcount: 12 
-            }, 
-            payloads::ConnectionInfo{
-                to_node: String::from(nodes[2]), 
-                hitcount: 420
-            }
-        ]
+        String::from(nodes[0]),
+        vec![
+            payloads::ConnectionInfo {
+                to_node: String::from(nodes[1]),
+                hitcount: 12,
+            },
+            payloads::ConnectionInfo {
+                to_node: String::from(nodes[2]),
+                hitcount: 420,
+            },
+        ],
     );
     let mut metadata = HashMap::new();
     metadata.insert(
-        String::from(nodes[0]), 
-        payloads::NodeInfo{
-            name: String::from("ServiceA"), 
-            description: String::from("root service"), 
-            uuid: String::from(nodes[0])}
+        String::from(nodes[0]),
+        payloads::NodeInfo {
+            name: String::from("ServiceA"),
+            description: String::from("root service"),
+            uuid: String::from(nodes[0]),
+        },
     );
     metadata.insert(
-        String::from(nodes[1]), 
-        payloads::NodeInfo{
-            name: String::from("ServiceB"), 
-            description: String::from("dependent service"), 
-            uuid: String::from(nodes[1])}
+        String::from(nodes[1]),
+        payloads::NodeInfo {
+            name: String::from("ServiceB"),
+            description: String::from("dependent service"),
+            uuid: String::from(nodes[1]),
+        },
     );
     metadata.insert(
-        String::from(nodes[2]), 
-        payloads::NodeInfo{
-            name: String::from("ServiceC"), 
-            description: String::from("other service"), 
-            uuid: String::from(nodes[2])}
+        String::from(nodes[2]),
+        payloads::NodeInfo {
+            name: String::from("ServiceC"),
+            description: String::from("other service"),
+            uuid: String::from(nodes[2]),
+        },
     );
     return Json(payloads::GraphPayload {
         adjacency_map,
-        metadata
-
+        metadata,
     });
 }
-
-
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .mount("/", routes![index])
-    .mount("/", routes![log_trace])
-    .mount("/", routes![get_graph])
+        .mount("/", routes![index])
+        .mount("/", routes![log_trace])
+        .mount("/", routes![get_graph])
 }
