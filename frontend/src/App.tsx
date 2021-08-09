81cdd34c281ad8de163c00effe711bb0fc677a2b
Alberto Leal <mail4alberto@gmail.com>
2021-08-09 12:53:05 -0400

    add tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,34 @@ import React from "react";
 
 import styled from "styled-components";
 
-const Component = styled.div`
-  background: blue;
-  color: red;
+import tw from "twin.macro";
+
+const StyledForm = styled.main.attrs({
+  className: "flex flex-col h-screen justify-center items-center bg-gray-100",
+})`
+  & {
+    form {
+      ${tw`bg-white text-center rounded py-8 px-5 shadow max-w-xs`}
+    }
+    input {
+      ${tw`border-gray-300 mb-4 w-full border-solid border rounded py-2 px-4`}
+    }
+    button {
+      ${tw`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`}
+    }
+  }
 `;
 
 function App() {
   return (
-    <div className="App">
-      <Component>foo</Component>
-    </div>
+    <StyledForm>
+      <form>
+        <input type="text" placeholder="Full name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <button>Sign In</button>
+      </form>
+    </StyledForm>
   );
 }
 
