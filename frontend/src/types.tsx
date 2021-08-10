41c74fc32dc3884aefd3fd4ba4bc5dd1f55f1d9e
Alberto Leal <mail4alberto@gmail.com>
2021-08-10 07:14:29 -0400

    add Graph ts type

---
+++ frontend/src/types.tsx
@@ -0,0 +1,22 @@
+export type Uuid = string;
+
+export type CombinedEdge = {
+  from_node_id: Uuid;
+  to_node_id: Uuid;
+  status_ok: number;
+  status_expected_error: number;
+  status_unexpected_error: number;
+};
+
+export type NodeType = "service" | "transaction";
+
+export type Node = {
+  node_id: Uuid;
+  node_type: NodeType;
+  name: String;
+};
+
+export type Graph = {
+  edges: Array<CombinedEdge>;
+  nodes: Array<Node>;
+};
