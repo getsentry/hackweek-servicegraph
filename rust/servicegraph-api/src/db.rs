cea9fbe1a4d471e1a6d93c57494e6a6d56d1efb9
Armin Ronacher <armin.ronacher@active-4.com>
2021-08-10 09:50:11 +0200

    ref: update schema

--- rust/servicegraph-api/src/db.rs
+++ rust/servicegraph-api/src/db.rs
@@ -6,7 +6,7 @@ use uuid::Uuid;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Row)]
 struct Node {
-    id: Uuid,
+    id: [u8; 16],
     service_name: String,
     transaction_name: String,
     description: String,
@@ -14,15 +14,15 @@ struct Node {
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, Row)]
 struct Connections {
-    checkin_time: u64,
-    src_scope: Uuid,
-    dst_scope: Uuid,
-    op_n: u32,
+    checkin_time: u32,
+    from_node_id: [u8; 16],
+    to_node_id: [u8; 16],
+    n: u32,
 }
 
 async fn register_node(client: &Client, node: payloads::NodeInfo) -> Result<()> {
     let row = Node {
-        id: node.uuid,
+        id: *node.uuid.as_bytes(),
         service_name: node.name,
         transaction_name: node.transaction,
         description: node.description,
@@ -32,16 +32,21 @@ async fn register_node(client: &Client, node: payloads::NodeInfo) -> Result<()> 
     insert.end().await
 }
 
-async fn insert_connections(client: &Client, src: Uuid, dst: Uuid, n: u32) -> Result<()> {
+async fn insert_connections(
+    client: &Client,
+    from_node_id: Uuid,
+    to_node_id: Uuid,
+    n: u32,
+) -> Result<()> {
     let now = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap()
         .as_secs();
     let row = Connections {
-        checkin_time: now,
-        src_scope: src,
-        dst_scope: dst,
-        op_n: n,
+        checkin_time: now as u32,
+        from_node_id: *from_node_id.as_bytes(),
+        to_node_id: *to_node_id.as_bytes(),
+        n,
     };
     let mut insert = client.insert("connections")?;
     insert.write(&row).await?;
@@ -60,14 +65,14 @@ async fn test_register_node() {
         name: String::from("ServiceA"),
         transaction: String::from(""),
         description: String::from("a service"),
-        uuid: String::from("418f3d00-ba14-42eb-98b8-5f3fb1b975c8"),
+        uuid: "418f3d00-ba14-42eb-98b8-5f3fb1b975c8".parse().unwrap(),
     };
 
     let node_two = payloads::NodeInfo {
         name: String::from("ServiceB"),
         transaction: String::from(""),
         description: String::from("another service"),
-        uuid: String::from("5042546b-07a0-41d4-a73c-9138722eebb4"),
+        uuid: "5042546b-07a0-41d4-a73c-9138722eebb4".parse().unwrap(),
     };
 
     let client = get_client();
@@ -78,7 +83,7 @@ async fn test_register_node() {
 #[tokio::test]
 async fn test_insert_connections() {
     let client = get_client();
-    let src = "418f3d00-ba14-42eb-98b8-5f3fb1b975c8";
-    let dst = "5042546b-07a0-41d4-a73c-9138722eebb4";
+    let src = "418f3d00-ba14-42eb-98b8-5f3fb1b975c8".parse().unwrap();
+    let dst = "5042546b-07a0-41d4-a73c-9138722eebb4".parse().unwrap();
     insert_connections(&client, src, dst, 20).await.unwrap();
 }
--- schema.sql
+++ schema.sql
@@ -11,43 +11,20 @@ CREATE TABLE IF NOT EXISTS servicegraph.nodes (
 ORDER BY (service_name, transaction_name, id);
 
 CREATE TABLE IF NOT EXISTS servicegraph.connections (
-    -- the timestamp of the checkin
     checkin_time DateTime,
-    -- the window size of the checkin in 10 second multiples.
-    checkin_window UInt8,
-    -- the id of the source scope
-    src_scope UUID,
-    -- the id of the destination scope
-    dst_scope UUID,
-    -- the status flag of the source
+    from_node_id UUID,
+    to_node_id UUID,
     src_status UInt8,
-    -- the status flag of the destination as observed by the source
-    src_observed_dst_status UInt8,
-    -- the type ID of the source
-    src_type UInt16,
-    -- the type ID of the destination (as reported by the source)
-    dst_type UInt16,
-    -- the load in the range of 0-255
-    src_load UInt8,
-    -- the type ID of the operation
-    op_type UInt16,
-    -- the N value of the operation (types the operation was performed)
-    op_n UInt32,
-    -- human readable source description
-    src_description String,
-    -- human readable destination description
-    dst_description String,
-    -- human readable operation description
-    op_description String
+    n UInt32
 ) ENGINE = MergeTree()
-PARTITION BY (toYYYYMMDD(checkin_time), src_scope)
+PARTITION BY (toYYYYMMDD(checkin_time), from_node_id)
 ORDER BY checkin_time;
 
 CREATE TABLE IF NOT EXISTS servicegraph.connections_by_minute (
     -- timestamp bucketed by minute
     checkin_time DateTime,
-    src_scope UUID,
-    dst_scope UUID,
+    from_node_id UUID,
+    to_node_id UUID,
     n UInt32
 ) ENGINE = SummingMergeTree()
 ORDER BY checkin_time;
@@ -55,10 +32,10 @@ ORDER BY checkin_time;
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS servicegraph.connections_by_minute_mv TO servicegraph.connections_by_minute (
     checkin_time DateTime,
-    src_scope UUID,
-    dst_scope UUID,
+    from_node_id UUID,
+    to_node_id UUID,
     n UInt32
 )
-AS SELECT toStartOfMinute(checkin_time) AS checkin_time, src_scope, dst_scope, sum(op_n) as n
+AS SELECT toStartOfMinute(checkin_time) AS checkin_time, from_node_id, to_node_id, sum(n) as n
 FROM servicegraph.connections
-GROUP BY src_scope, dst_scope, checkin_time;
+GROUP BY from_node_id, to_node_id, checkin_time;
