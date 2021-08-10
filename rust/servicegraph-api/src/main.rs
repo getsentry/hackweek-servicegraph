26205549fbbb57f3da0fde4c2fb2de6b8aea8f59
Alberto Leal <mail4alberto@gmail.com>
2021-08-10 07:37:59 -0400

    add cors

--- rust/servicegraph-api/src/main.rs
+++ rust/servicegraph-api/src/main.rs
@@ -6,7 +6,36 @@ mod db;
 mod endpoints;
 mod error;
 
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+// https://stackoverflow.com/questions/62412361/how-to-set-up-cors-or-options-for-rocket-rs
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![endpoints::submit, endpoints::query])
+    rocket::build()
+        .attach(CORS)
+        .mount("/", routes![endpoints::submit, endpoints::query])
 }
