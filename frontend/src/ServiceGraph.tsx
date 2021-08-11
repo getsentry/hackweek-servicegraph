c115c115ce564ad6a5816da5bbec060f38413c6f
Alberto Leal <mail4alberto@gmail.com>
2021-08-10 20:58:30 -0400

    cytoscape hack

--- frontend/src/ServiceGraph.tsx
+++ frontend/src/ServiceGraph.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import cytoscape from "cytoscape";
+// @ts-expect-error
+import fcose from "cytoscape-fcose";
 import * as ReactD3Graph from "react-d3-graph";
 import styled from "styled-components";
 import { useQuery } from "react-query";
@@ -70,9 +73,6 @@ function processServiceGraphData(
   serviceGraphData: Graph
 ): ReactD3Graph.GraphData<ServiceGraphNode, ServiceGraphLink> {
   const nodes: ServiceGraphNode[] = serviceGraphData.nodes.map((node) => {
-    if (node.parent_id) {
-      console.log("node", node);
-    }
     return {
       ...node,
       id: node.node_id,
@@ -96,6 +96,9 @@ function processServiceGraphData(
 function ServiceGraph() {
   const windowSize = useWindowSize();
 
+  const serviceGraphContainerElement = React.useRef<HTMLDivElement>(null);
+  const graph = React.useRef<cytoscape.Core>();
+
   const [details, setDetails] = React.useState<DetailsPayload | undefined>(
     undefined
   );
@@ -106,9 +109,194 @@ function ServiceGraph() {
     fetchServiceGraph,
     {
       // Refetch the data every second
-      refetchInterval: 1000,
+      // refetchInterval: 1000,
     }
   );
+
+  React.useEffect(() => {
+    if (!serviceGraphContainerElement.current) {
+      return;
+    }
+
+    try {
+      if (!graph.current || graph.current.destroyed()) {
+        if (graph.current?.destroyed()) {
+          console.log("graph destroyed; recreating");
+        }
+        cytoscape.use(fcose);
+
+        let nodes: cytoscape.NodeDefinition[] = [];
+        let edges: cytoscape.EdgeDefinition[] = [];
+
+        if (data) {
+          nodes = data.nodes.map((node): cytoscape.NodeDefinition => {
+            return {
+              data: {
+                ...node,
+                id: node.node_id,
+                parent: node.parent_id,
+              },
+            };
+          });
+
+          edges = data.edges.map((edge): cytoscape.EdgeDefinition => {
+            return {
+              data: {
+                ...edge,
+                source: edge.from_node_id,
+                target: edge.to_node_id,
+              },
+            };
+          });
+        }
+
+        graph.current = cytoscape({
+          // @ts-expect-error
+          ready: function () {
+            const core = this as unknown as cytoscape.Core;
+            if (!core) {
+              return;
+            }
+            core
+              .layout({
+                name: "fcose",
+                idealEdgeLength: () => 200,
+              } as any)
+              .run();
+          },
+          elements: {
+            nodes: [
+              ...nodes,
+              // { data: { id: "n1" } },
+              // { data: { id: "n2" } },
+              // { data: { id: "n3", parent: "n8" } },
+              // { data: { id: "n5" } },
+              // { data: { id: "n6", parent: "n8" } },
+              // { data: { id: "n7", parent: "n8" } },
+              // { data: { id: "n8" } },
+              // { data: { id: "f1" } },
+              // { data: { id: "f2" } },
+              // { data: { id: "f3", parent: "n8" } },
+            ],
+            edges: [
+              ...edges,
+              // { data: { source: "n1", target: "f1" } },
+              // { data: { source: "n1", target: "n3" } },
+              // { data: { source: "f1", target: "n2" } },
+              // { data: { source: "f1", target: "n3" } },
+              // { data: { source: "n3", target: "f2" } },
+              // { data: { source: "f2", target: "n5" } },
+              // { data: { source: "n5", target: "n8" } },
+              // { data: { source: "n6", target: "n3" } },
+              // { data: { source: "n6", target: "n7" } },
+              // { data: { source: "n6", target: "f3" } },
+            ],
+          },
+          minZoom: 0.3,
+          maxZoom: 1,
+          style: [
+            {
+              selector: "node",
+              style: {
+                "background-color": "#bdd3d4",
+                label: "data(name)",
+                "text-valign": "bottom",
+                "background-opacity": 0.7,
+              },
+            },
+
+            {
+              selector: ":parent",
+              style: {
+                //      'background-opacity': 0.333,
+                "background-color": "#e8e8e8",
+                "border-color": "#DADADA",
+                //      'border-width': 3,
+                "text-valign": "bottom",
+              },
+            },
+
+            {
+              selector: "edge",
+              style: {
+                "curve-style": "straight",
+                "line-color": "#bdd3d4",
+                "target-arrow-shape": "triangle",
+              },
+            },
+
+            {
+              selector: "node:selected",
+              style: {
+                "background-color": "#33ff00",
+                "border-color": "#22ee00",
+              },
+            },
+
+            {
+              selector: "node.fixed",
+              style: {
+                shape: "diamond",
+                "background-color": "#9D9696",
+              },
+            },
+
+            {
+              selector: "node.fixed:selected",
+              style: {
+                "background-color": "#33ff00",
+              },
+            },
+
+            {
+              selector: "node.alignment",
+              style: {
+                shape: "round-heptagon",
+                "background-color": "#fef2d1",
+              },
+            },
+
+            {
+              selector: "node.alignment:selected",
+              style: {
+                "background-color": "#33ff00",
+              },
+            },
+
+            {
+              selector: "node.relative",
+              style: {
+                shape: "rectangle",
+                "background-color": "#fed3d1",
+              },
+            },
+
+            {
+              selector: "node.relative:selected",
+              style: {
+                "background-color": "#33ff00",
+              },
+            },
+
+            {
+              selector: "edge:selected",
+              style: {
+                "line-color": "#33ff00",
+              },
+            },
+          ],
+          // wheelSensitivity: 0.2,
+          container: serviceGraphContainerElement.current,
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    return () => {
+      graph.current && graph.current.destroy();
+    };
+  }, [data, error, isLoading]);
 
   if (isLoading) {
     return <Container>Loading</Container>;
@@ -169,7 +357,8 @@ function ServiceGraph() {
 
   return (
     <React.Fragment>
-      <Container>
+      <Container ref={serviceGraphContainerElement} />
+      <Container style={{ display: "none" }}>
         <ReactD3Graph.Graph
           key="service-graph"
           id="service-graph"
@@ -185,6 +374,7 @@ function ServiceGraph() {
           onClickLink={onClickLink}
         />
       </Container>
+
       <DetailsPanel>
         <Details details={details} />
       </DetailsPanel>
@@ -240,10 +430,9 @@ function Details(props: { details: DetailsPayload | undefined }) {
 
 const Container = styled.div`
   ${tw`rounded shadow-lg bg-white`};
-
-  & > div[id="service-graph-graph-wrapper"] > svg {
-    width: 100vw;
-  }
+  width: 100vw;
+  height: 70vh;
+  outline: 1px solid red;
 `;
 
 const DetailsPanel = styled.div`
